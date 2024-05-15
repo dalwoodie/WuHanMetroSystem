09e9534ff6a948278ce245e3f1bab20e8ef54d8a
dalwoodie <zx20435@foxmail.com>
2024-05-15 12:08:17 +0800

    问题1~2已完成

---
+++ src/station/Station.java
@@ -0,0 +1,50 @@
+package station;
+
+import java.util.Objects;
+
+public class Station {
+    private String name;
+    private boolean visited;
+
+    public Station() {
+    }
+
+    public Station(String name) {
+        this.name = name;
+        this.visited = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Station station = (Station) o;
+        return Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
